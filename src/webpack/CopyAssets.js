8f7c15845f48f1bbe6fba04305af1ea8d89676e4
radu <radu.helstern@deskpro.com>
2017-04-19 16:01:55 +0100

    update copy assets plugin

--- src/webpack/CopyAssets.js
+++ src/webpack/CopyAssets.js
@@ -8,17 +8,21 @@ const PROJECT_ROOT_PATH = path.resolve(__dirname, '../../');
 /**
  * @return {[*,*]}
  */
-function getCopyCommands()
+function getCopyCommands(destination)
 {
+    if (-1 === ['dist','target'].indexOf(destination)) {
+        throw new Error('destination must one of: dist, target');
+    }
+
     return [
         {
-            from: path.resolve(PROJECT_ROOT_PATH, 'src/main/html'),
-            to: path.join(PROJECT_ROOT_PATH, 'target/html'),
+            from: path.resolve(PROJECT_ROOT_PATH, 'src', 'main', 'html'),
+            to: path.join(PROJECT_ROOT_PATH, destination, 'html'),
             force: true
         },
         {
-            from: path.resolve(PROJECT_ROOT_PATH, 'src/main/resources'),
-            to: path.join(PROJECT_ROOT_PATH, 'target/assets'),
+            from: path.resolve(PROJECT_ROOT_PATH, 'src', 'main', 'resources'),
+            to: path.join(PROJECT_ROOT_PATH, destination, 'assets'),
             force: true
         }
     ];
@@ -30,19 +34,29 @@ function getCopyCommands()
  * @return {String}
  */
 function transformHtml(content, path) {
+    console.log('null transform');
     return content;
 }
 
 /**
- * @param {Array} copyCommands
+ * @param {Array|String} copyCommands
  */
 function createPlugin(copyCommands)
 {
-    const commands = copyCommands instanceof Array ? copyCommands : getCopyCommands();
-    const options = { debug: true, force: true };
+    let commands;
+    if (typeof copyCommands === 'string') {
+      commands = getCopyCommands(copyCommands);
+    } else if (copyCommands instanceof Array && copyCommands.length) {
+        commands = copyCommands;
+    } else {
+        throw new Error('unexpected argument. Expecting either a destination or an array of copy commands');
+    }
 
+    const options = { debug: true, force: true };
     return new CopyWebpackPlugin(commands, options);
 }
+
+
 
 module.exports = {
     getCopyCommands: getCopyCommands,
